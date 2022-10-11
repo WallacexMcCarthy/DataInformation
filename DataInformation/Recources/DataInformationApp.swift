27a4618bfd609e30eaa77728e7f11b3e816fc756
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-10-11 07:38:16 -0600

    Initial Commit

---
+++ DataInformation/Recources/DataInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataInformationApp.swift
+//  DataInformation
+//
+//  Created by Mccarthy, Wallace on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DataInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
