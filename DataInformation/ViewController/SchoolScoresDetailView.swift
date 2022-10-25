9e7f594bb9369435ddbee323eb49264332fcebd0
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-10-25 08:53:58 -0600

    added accessibility

--- DataInformation/ViewController/SchoolScoresDetailView.swift
+++ DataInformation/ViewController/SchoolScoresDetailView.swift
@@ -9,21 +9,50 @@ import SwiftUI
 
 struct SchoolScoresDetailView: View
 {
-//    var detailedSchoolScores : SchoolScrores
-    
+    var detailedSchoolScores : SchoolScrores
     
     var body: some View
     {
-        HStack
+        VStack
         {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            VStack
+            Text("School Scores Item Detail")
+            HStack
             {
-                Text("1")
-                Text("2")
-                Text("3")
-                Text("4")
-            }
+                Text("State:\n\(detailedSchoolScores.State)")
+                    .padding()
+                    .accessibilityLabel("School Scores Item Title")
+                    .accessibilityValue("School Scores Item Detail")
+                VStack
+                {
+                    Text("Total Test Takers: \(detailedSchoolScores.totalTestTakers)")
+                        .accessibilityLabel("Total Test Takers")
+                        .accessibilityValue("Total Test Takers: \(detailedSchoolScores.totalTestTakers)")
+                    Text("Female Test Takers: \(detailedSchoolScores.femaleTestTakers)")
+                        .accessibilityLabel("Female Test Takers")
+                        .accessibilityValue("Female Test Takers: \(detailedSchoolScores.femaleTestTakers)")
+                    Text("Male Test Takers: \(detailedSchoolScores.maleTestTakers)")
+                        .accessibilityLabel("Male Test Takers")
+                        .accessibilityValue("Male Test Takers: \(detailedSchoolScores.maleTestTakers)")
+                    Text("Average Preforming Arts GPA: \(detailedSchoolScores.preformingArtsGPA)")
+                        .accessibilityLabel("Average Preforming Arts GPA")
+                        .accessibilityValue("Average Preforming Arts GPA: \(detailedSchoolScores.preformingArtsGPA)")
+                    Text("Average English GPA: \(detailedSchoolScores.englishGPA)")
+                        .accessibilityLabel("Average English GPA")
+                        .accessibilityValue("Average English GPA: \(detailedSchoolScores.englishGPA)")
+                    Text("Average Forign Language GPA: \(detailedSchoolScores.forignLanguageGPA)")
+                        .accessibilityLabel("Average Forign Language GPA")
+                        .accessibilityValue("Average Forign Language GPA: \(detailedSchoolScores.forignLanguageGPA)")
+                    Text("Average Mathematics GPA: \(detailedSchoolScores.mathematicsGPA)")
+                        .accessibilityLabel("Average Mathematics GPA")
+                        .accessibilityValue("Average Mathematics GPA: \(detailedSchoolScores.mathematicsGPA)")
+                    Text("Average Natural Sciences GPA: \(detailedSchoolScores.naturalSciencesGPA)")
+                        .accessibilityLabel("Average Naturarl Sciences GPA")
+                        .accessibilityValue("Average Natural Sciences GPA: \(detailedSchoolScores.naturalSciencesGPA)")
+                    Text("Average Social Sciences GPA: \(detailedSchoolScores.socialSciencesGPA)")
+                        .accessibilityLabel("Average Social Sciences GPA")
+                        .accessibilityValue("Average Social Sciences GPA: \(detailedSchoolScores.socialSciencesGPA)")
+                }
+        }
         }
     }
 }
@@ -32,6 +61,6 @@ struct schoolScoresDetailView_Previews: PreviewProvider
 {
     static var previews: some View
     {
-        SchoolScoresDetailView()
+        SchoolScoresDetailView(detailedSchoolScores: demoSchoolScores)
     }
 }
