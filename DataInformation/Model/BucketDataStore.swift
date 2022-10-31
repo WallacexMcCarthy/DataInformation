d759d96a71016c3b747dbbcae66c4e59dd3ed4b6
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-10-31 08:11:38 -0600

    edit data stored on device function

--- DataInformation/Model/BucketDataStore.swift
+++ DataInformation/Model/BucketDataStore.swift
@@ -24,6 +24,18 @@ class BucketDataStore: ObservableObject
     
     init(buckets: [BucketListItem])
     {
+        if let storedOnDevice = UserDefaults.standard.data(forKey: "Saved Buckets")
+        {
+            let decoder = JSONDecoder()
+            if let savedBuckets = try? decoder.decode([BucketListItem].self, from: storedOnDevice)
+            {
+                if (savedBuckets.count != 0)
+                {
+                    self.buckets = savedBuckets
+                    return // Early return since we are loading data for the device
+                }
+            }
+        }
         self.buckets = buckets
     }
     
