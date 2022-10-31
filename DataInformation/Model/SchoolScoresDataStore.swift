b2d68f01afdbfaef384d6beed055f4007c63f3b2
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-10-31 08:32:15 -0600

    added remove items for school scores

--- DataInformation/Model/SchoolScoresDataStore.swift
+++ DataInformation/Model/SchoolScoresDataStore.swift
@@ -11,9 +11,33 @@ import Combine
 class SchoolScoresDataStore : ObservableObject
 {
     @Published var schoolData : [SchoolScrores]
+    {
+        didSet
+        {
+            let encoder = JSONEncoder()
+            if let encodedSchool = try? encoder.encode(schoolData)
+            {
+                UserDefaults.standard.set(encodedSchool, forKey: "Saved School Scores")
+            }
+        }
+    }
+    
+    
     
     init (schoolData : [SchoolScrores])
     {
+        if let storedOnDevice = UserDefaults.standard.data(forKey: "Saved School Scores")
+        {
+            let decoder = JSONDecoder()
+            if let savedScores = try? decoder.decode([SchoolScrores].self, from: storedOnDevice)
+            {
+                if (savedScores.count != 80)
+                {
+                    self.schoolData = savedScores
+                    return
+                }
+            }
+        }
         self.schoolData = schoolData
     }
 }
